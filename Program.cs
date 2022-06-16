47cdc3180b6ea545dff3e27755be483f7a8a0740
behzad.b.i.g@gmail.com <behza@DESKTOP-62KEJTU>
2022-06-16 20:12:00 +0430

    commit of question number one and number two

--- Program.cs
+++ Program.cs
@@ -6,10 +6,42 @@ namespace Tamrin7
     {
         static void Main(string[] args)
         {
-           
+            ////Soal1
+            string word = Console.ReadLine();
+
+            string reverseWord = string.Empty;
+
+            string answer = ReverseString(word, reverseWord, word.Length);
+
+            Console.WriteLine(answer);
+            ////End Of Soal 1 
+            ///Soal 2
+            string stringHolder = string.Empty;
+
+            Console.WriteLine("Input a string ");
+
+            stringHolder = Console.ReadLine();
+            bool trueOfFalse = PalindromeString(stringHolder);
+            if (trueOfFalse)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+
         }
     
-    
+        /// <summary>
+        /// Soal 1 Soal bar ax kardane string
+        /// سوال وسط هفته
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="TheWordRevesed"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
         public static string ReverseString(string word,string TheWordRevesed,int index)
         {
             if (index > 0)
@@ -23,6 +55,50 @@ namespace Tamrin7
                 return TheWordRevesed;
             }
         }
+  
+    
+        /// <summary>
+        /// Soal 1 In Hafte
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="powerOf"></param>
+        /// <returns></returns>
+        private static float Pow(float number,int powerOf)
+        {
+            if (powerOf == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Pow(number, powerOf - 1);
+            }
+
+        }
+    
+        
+        public static bool PalindromeString(string txt)
+        {
+            if (txt.Length <= 1)
+            {
+                return true;
+            }
+            else
+            {
+                if (txt[0] != txt[txt.Length - 1])
+                {
+                    return false;
+                }
+                else
+                {
+                    return PalindromeString(txt.Substring(1, txt.Length - 2));
+                }
+                
+            }
+        }
+    
+
+
     }
 
 }
