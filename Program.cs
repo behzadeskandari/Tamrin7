6a9d19b501e9ef3af99181a7cc62dae8bde131f3
behzad.b.i.g@gmail.com <behza@DESKTOP-62KEJTU>
2022-06-16 19:08:03 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tamrin7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    
+    
+        public static string ReverseString(string word,string TheWordRevesed,int index)
+        {
+            if (index > 0)
+            {
+                index--;
+                TheWordRevesed = TheWordRevesed + word[index];
+                return ReverseString(word, TheWordRevesed, index);
+            }
+            else
+            {
+                return TheWordRevesed;
+            }
+        }
+    }
+
+}
